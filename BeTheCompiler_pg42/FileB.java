a062c61a6e265e1548b867596fc78a7aa553008a
Ubuntu 12.04.4 Desktop i386 <haq.asad000@gmail.com>
2015-05-28 17:25:32 -0700

    adding examples of java

---
+++ BeTheCompiler_pg42/FileB.java
@@ -0,0 +1,22 @@
+class DVDPlayer {
+	
+	boolean canRecord = false;
+
+	void recordDVD() {
+		System.out.println("DVD recording");
+	}
+}
+
+class FileB {
+
+	public static void main(String [] args) {
+
+		DVDPlayer d = new DVDPlayer();
+		d.canRecord = true;
+		d.recordDVD();
+
+		if (d.canRecord == true) {
+			d.recordDVD();
+		}
+	}
+}
